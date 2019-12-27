ddf29828cde40a98dbf56df271782141c9a18122
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-12-27 23:45:44 +0800

    feature: Add Footer

--- js/app.js
+++ js/app.js
@@ -2,3 +2,6 @@
 particlesJS.load('particles-js', 'js/assets/particles.json', function () {
   console.log('callback - particles.js config loaded');
 });
+
+// Replace footer copyright year with current year
+document.querySelector('#year').textContent = new Date().getFullYear();
